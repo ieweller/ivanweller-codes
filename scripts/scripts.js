f07d9584181d41f02a162523238331b12594d8f1
Ivan Weller <ivan@wellerindependent.com>
2020-11-10 13:05:48 -0800

    updated button hover styles, changed order of portfolio items

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,3 +1,7 @@
+// $('.js-tilt').tilt({
+//   maxTilt: 8,
+// });
+
 $(function() {
   $.ajax({
     type : 'GET',
@@ -7,10 +11,18 @@ $(function() {
     dataType : 'json',
     success : function(result) {
       createList(result);
-      filterSelection("all")
+      filterSelection("all");
+      $.getScript("scripts/tilt.jquery.min.js");
     }
   });
 });
+
+
+// Toggles what's being displayed in the hero area
+function showDetail() {
+  $('.hero-content').toggleClass("hide");
+  $('.about-me').toggleClass("hide");
+}
 
 function createList(listArray) {
 
@@ -26,7 +38,7 @@ function createList(listArray) {
     for(var i=0; i < val.tags.length; i++){
       buffer += ' ' + val.tags[i];
     }
-    buffer += '"><img src="img/';
+    buffer += '" data-tilt data-tilt-maxTilt="35" data-tilt-perspective="2000" data-tilt-glare="true" data-tilt-maxGlare=".5" data-tilt-speed="1200" data-tilt-scale="1.05"><img src="img/';
     buffer += val.thumbnail;
     buffer += '" alt=""></a>';
   });
@@ -34,7 +46,46 @@ function createList(listArray) {
   $('.portfolio-grid').html(buffer);
 }
 
-function showDetail() {
-  $('.hero-content').toggleClass("hide");
-  $('.about-me').toggleClass("hide");
+// Strip "show" class from all thumbnails, add to filtered thumbnails
+function filterSelection(filter) {
+  var thumbArr = document.getElementsByClassName("portfolio-grid__thumbnail");
+  if (filter == "all") filter = "";
+  for (var i = 0; i < thumbArr.length; i++) {
+    removeClass(thumbArr[i], "show");
+    if (thumbArr[i].className.indexOf(filter) > -1) addClass(thumbArr[i], "show");
+  }
+}
+
+// Show selected thumbnails
+function addClass(element, name) {
+  var classNameArr = element.className.split(" ");
+  var nameArr = name.split(" ");
+  for (var i = 0; i < nameArr.length; i++) {
+    if (classNameArr.indexOf(nameArr[i]) == -1) {
+      element.className += " " + nameArr[i];
+    }
+  }
+}
+
+// Hide unselected thumbnails
+function removeClass(element, name) {
+  var classNameArr = element.className.split(" ");
+  var nameArr = name.split(" ");
+  for (var i = 0; i < nameArr.length; i++) {
+    while (classNameArr.indexOf(nameArr[i]) > -1) {
+      classNameArr.splice(classNameArr.indexOf(nameArr[i], 1));
+    }
+  }
+  element.className = classNameArr.join(" ");
+}
+
+// Highlight current filter
+var btnFilter = document.getElementById("filter-bar");
+var btns = btnFilter.getElementsByClassName("filter-btn");
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function() {
+    var current = document.getElementsByClassName("selected");
+    current[0].className = current[0].className.replace(" selected", "");
+    this.className += " selected";
+  });
 }
